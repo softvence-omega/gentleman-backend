2c5f5171567cee16880e8921fb8591faf4ee1d94
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-05-30 03:36:52 +0600

    remove prisma database

--- src/modules/user/service/service.service.ts
+++ src/modules/user/service/service.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ServiceService {
-  constructor(private prisma: PrismaService) {}
-}
+export class ServiceService {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ControllerController } from './controller/controller.controller';
 import { ServiceService } from './service/service.service';
-import { PrismaModule } from 'src/prisma/ prisma.module';
 
 @Module({
   controllers: [ControllerController],
   providers: [ServiceService],
-  imports: [PrismaModule],
 })
 export class UserModule {}
