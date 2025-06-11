87b372575965aa29e9436ccc519fe6814b8bd6e2
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-06-11 06:21:44 +0600

    refctor booking

--- src/modules/booking/controller/booking.controller.ts
+++ src/modules/booking/controller/booking.controller.ts
@@ -5,6 +5,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   Req,
   UploadedFiles,
   UseInterceptors,
@@ -14,6 +15,7 @@ import { CreateBookingDto } from '../dto/create-booking.dto';
 import { UpdateBookingDto, UpdateBookingStatusDto, UpdateBookingWorkStatusDto, UpdatePaymentStatusDto } from '../dto/update-booking.dto';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
+import { ApiQuery } from '@nestjs/swagger';
 
 
 @Controller('bookings')
@@ -37,18 +39,39 @@ async createBooking(
   return this.bookingService.createBooking(dto,userId ,files.image || [] ,files?.vehicleImage?.[0] );
 }
 
+   @Get('/locations')
+  getallLocations(@Param('id') id: string) {
+    return this.bookingService.findAllLocations()
+  }
+
+
+
+   @Get("allBooking")
+  @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+  @ApiQuery({ name: 'order', required: false, enum: ['ASC', 'DESC'], example: 'DESC' })
+  async getAllBookings(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('order') order: 'ASC' | 'DESC' = 'DESC',
+  ) {
+    return this.bookingService.getAllBookings(page, limit, order);
+  }
+
+ 
+
 
   @Patch(':id')
   updateBooking(@Param('id') id: string, @Body() dto: UpdateBookingDto) {
     return this.bookingService.updateBooking(id, dto);
   }
 
-  @Patch(':id/status')
+  @Patch('status/:id')
   updateBookingStatus(@Param('id') id: string, @Body() dto: UpdateBookingStatusDto) {
     return this.bookingService.updateBookingStatus(id, dto);
   }
 
-  @Patch(':id/work-status')
+  @Patch('work-status/:id')
   updateWorkStatus(@Param('id') id: string, @Body() dto: UpdateBookingWorkStatusDto) {
     return this.bookingService.updateWorkStatus(id, dto);
   }
@@ -59,12 +82,29 @@ async createBooking(
   }
 
   @Get('pending')
-  getPendingBookings() {
-    return this.bookingService.getPendingBookings();
+  getPendingBookings( @Req() req) {
+     const userId = req.user.userId
+    return this.bookingService.getPendingBookings(userId);
+  }
+  @Get('completed')
+  getCompletedBooking( @Req() req) {
+     const userId = req.user.userId
+    return this.bookingService.getCompletedBookings(userId);
   }
 
   @Get(':id')
   getBookingById(@Param('id') id: string) {
     return this.bookingService.getBookingById(id);
   }
+  @Patch('cancelBooking/:id')
+  cancelBooking(@Param('id') id: string) {
+    return this.bookingService.cancelBooking(id)
+  }
+
+
+
+
+
+
+  
 }
